5da1c5fe06d322420126cf491a74e70810cecb35
juan felipe <jf@duartte.co>
2025-05-26 17:31:00 +0200

    nuke

--- config/routes.rb
+++ config/routes.rb
@@ -6,7 +6,6 @@ Rails.application.routes.draw do
   # Can be used by load balancers and uptime monitors to verify that the app is live.
   get "up" => "rails/health#show", as: :rails_health_check
   resources :posts do
-    resources :comments, only: [:create]
+    resources :comments, only: [ :create ]
   end
 end
-
